004b8047a783844d6cc40c8047cc79b6db609c8b
Marcus Jøsendal <marcusjosendal@Marcuss-MacBook-Pro.local>
2019-11-16 15:10:11 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,36 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+// Firebase-spesifikke moduler
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireAuthGuardModule } from '@angular/fire/auth-guard';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQiaByENUukJv-SGWyri_g1lzHH-vsIuY',
+  authDomain: 'roombook-e1ebf.firebaseapp.com',
+  databaseURL: 'https://roombook-e1ebf.firebaseio.com',
+  projectId: 'roombook-e1ebf',
+  storageBucket: 'roombook-e1ebf.appspot.com',
+  messagingSenderId: '428331030982',
+  appId: '1:428331030982:web:470f8a840555150b5e7ac7'
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+      BrowserModule,
+      IonicModule.forRoot(),
+      AppRoutingModule,
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFirestoreModule,
+      AngularFireAuthModule,
+      AngularFireStorageModule,
+      AngularFireAuthGuardModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
